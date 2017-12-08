9b30641ce2b3bdf4199f65ee6d6d5617d1d171cd
Larry Zhang <lzhang124@gmail.com>
2017-12-07 21:02:21 -0500

    better color

--- color.sh
+++ color.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-declare -a col=("rgb-lin"
-    "rgb-eig"
-    "rgb-chol"
-    "rgb-pca"
-    "rgb-rot"
-    "ycbcr-lin"
-    "ycbcr-eig"
-    "ycbcr-chol"
-    "ycbcr-pca"
-    "ycbcr-rot"
-    "lab-lin"
-    "lab-eig"
-    "lab-chol"
-    "lab-pca"
-    "lab-rot")
+col='rgb-lin
+    rgb-eig
+    rgb-chol
+    rgb-pca
+    rgb-rot
+    ycbcr-lin
+    ycbcr-eig
+    ycbcr-chol
+    ycbcr-pca
+    ycbcr-rot
+    lab-lin
+    lab-eig
+    lab-chol
+    lab-pca
+    lab-rot'
 
-python neural_style.py --content content/flowers_small.jpg --styles style/Robert_Delaunay,_1906,_Portrait.jpg --output output/flowers-luminance.jpg --luminance-transfer
+# python neural_style.py --content content/flowers_small.jpg --styles style/Robert_Delaunay,_1906,_Portrait.jpg --output output/flowers-luminance.jpg --luminance-transfer
 
-for i in "${col[@]}"
+for c in $col
 do
-   python neural_style.py --content content/flowers_small.jpg --styles style/Robert_Delaunay,_1906,_Portrait.jpg --output output/flowers-"-i".jpg --map-colors "-i"
+   python neural_style.py --content content/flowers_small.jpg --styles style/Robert_Delaunay,_1906,_Portrait.jpg --output output/flowers-$c.jpg --map-colors $c
 done
