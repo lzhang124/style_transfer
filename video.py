2a74e4e78860f564bb0cb604414eb7a601c97ffc
Ubuntu <ubuntu@ip-172-31-2-46.us-east-2.compute.internal>
2017-12-08 02:55:00 +0000

    results

--- video.py
+++ video.py
@@ -146,12 +146,12 @@ if __name__ == '__main__':
 
     options.initial = None
     options.content = prefix+frames[0]
-    options.output = "content/panda/out/p_0.jpg"
+    options.output = "content/panda/out/0.jpg"
     for i in range(1,len(frames)):
         neural_style.main(parser, options)
         options.initial = options.output
         options.content = prefix+frames[i]
-        options.output = "content/panda/out/p_"+str(i)+".jpg"
+        options.output = "content/panda/out/"+str(i)+".jpg"
     neural_style.main(parser, options)
 
 
